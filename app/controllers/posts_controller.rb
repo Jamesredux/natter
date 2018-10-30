6e7d859fac6a362f696fed2d01172840a994f9a6
James McCann <jamesredux@gmail.com>
2018-10-30 21:17:56 +0700

    create new post template view done 4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,28 @@
 class PostsController < ApplicationController
+	before_action :logged_in_user, only: [:new, :create]
+
   def new
+  	@post = Post.new
   end
+
+  def create
+  	@post = Post.new(post_params)
+  	if @post.save
+  		flash[:info] = "New post saved."
+  		redirect_to root_url
+  	else
+  		render 'new'
+  	end		
+  end
+
+  def destroy
+  	
+  end
+
+  private
+
+  def post_params
+  	params.require(:post).permit(:title, :body)
+  end
+
 end
