353b96ac068f3bbd26b7bf5b6235d876865e29b3
James McCann <jamesredux@gmail.com>
2018-10-31 21:05:42 +0700

    posts added 567 need tidying

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,13 @@ class PostsController < ApplicationController
   	@post = Post.new
   end
 
+  def index
+    @posts = Post.all
+  end
+
   def create
   	@post = Post.new(post_params)
+    @post.user_id = current_user.id
   	if @post.save
   		flash[:info] = "New post saved."
   		redirect_to root_url
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
 
-  root 		'pages#home'
+  root 		'posts#index'
 
   get 		'pages/home'
   get 		'/about', 		to: 'pages#about'
